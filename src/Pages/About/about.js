e6b7f09788ef502dd5307032dc1ca56ae8c6d2d5
Xongotelo Baloyi <xongotelob@gmail.com>
2025-01-21 20:14:45 +0200

    added image for about page

--- src/Pages/About/about.js
+++ src/Pages/About/about.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import image from "../../assets/about_image.jpg";
 
 const About = () => {
     return (
-        <section>
-            
-       </section>   
+      <section>
+        <div className="aboutDescription">
+          <span className="aboutWord">
+            Hello,
+            <span className="aboutText">
+              I'm
+              <span className="aboutName">
+                Xongotelo Baloyi
+                <br/>FullStack Developer
+              </span>
+            </span>
+          </span>
+        </div>
+        <img src={image} alt="profile photo" className="aboutImage"></img>
+      </section>
     );
 }
 
